cd1ee4937de45fe564df372b37615751c5feca9a
Damien Lebrun <dinoboff@hotmail.com>
2016-01-14 19:45:05 +0000

    Change Java implementation

--- verifiers/java/tests/com/singpath/cli/MainTest.java
+++ verifiers/java/tests/com/singpath/cli/MainTest.java
@@ -12,18 +12,29 @@ public class MainTest {
 
     @Test
     public void testProcessYaml() throws Exception {
-        String pauload = "---\n"
-                + "solution: |\n"
-                + "  public class SingPath {\n"
-                + "     public Double add() {\n"
-                + "        return 2.0;\n"
-                + "     }\n"
-                + "  } \n"
-                + "tests: |\n"
-                + "  SingPath sp = new SingPath();\n"
-                + "  assertEquals(2.0, 2.0);\n";
+        String payload = "---\n"
+            + "tests: |\n"
+            + "  import org.junit.Test;\n"
+            + "  import static org.junit.Assert.*;\n"
+            + "  import junit.framework.*;\n"
+            + "  import com.singpath.SolutionRunner;\n"
+            + "  \n"
+            + "  public class SingPathTest extends SolutionRunner {\n"
+            + "    @Test\n"
+            + "    public void testCapitalize() throws Exception {\n"
+            + "      SingPath sp = new SingPath();\n"
+            + "      assertEquals(2.0, sp.add());\n"
+            + "    }\n"
+            + "  }\n"
+            + "solution: |\n"
+            + "  public class SingPath {\n"
+            + "    public Double add() {\n"
+            + "      return 2.0;\n"
+            + "    }\n"
+            + "  }\n";
 
-        Response resp = Main.processReq(pauload);
+        Response resp = Main.processReq(payload);
+
         JSONObject dict = (JSONObject) JSONValue.parse(resp.toString());
         boolean solved = (boolean) dict.get("solved");
 
@@ -32,19 +43,12 @@ public class MainTest {
 
     @Test
     public void testProcessJSON() throws Exception {
-        String pauload = "{"
-                + "\"solution\": "
-                + "  \"public class SingPath {\\n"
-                + "     public Double add() {\\n"
-                + "        return 2.0;\\n"
-                + "     }\\n"
-                + "  }\","
-                + "\"tests\": "
-                + "\"SingPath sp = new SingPath();\\n"
-                + "assertEquals(2.0, 2.0);\\n\""
-                + "}";
+        String payload = "{\n"
+            + "  \"tests\": \"import org.junit.Test;\\nimport static org.junit.Assert.*;\\nimport junit.framework.*;\\nimport com.singpath.SolutionRunner;\\n\\npublic class SingPathTest extends SolutionRunner {\\n  @Test\\n  public void testCapitalize() throws Exception {\\n    SingPath sp = new SingPath();\\n    assertEquals(2.0, sp.add());\\n  }\\n}\n\","
+            + "  \"solution\": \"public class SingPath {\\n  public Double add() {\\n    return 2.0;\\n  }\\n}\\n\"\n"
+            + "}";
 
-        Response resp = Main.processReq(pauload);
+        Response resp = Main.processReq(payload);
         JSONObject dict = (JSONObject) JSONValue.parse(resp.toString());
         boolean solved = (boolean) dict.get("solved");
 
